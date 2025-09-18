03cd9e38f87d2920d63ef0f036dbdf24fdf4dd19
NoheilaRamdani <noheila.ramdani@etudiant.univ-reims.fr>
2025-09-18 10:05:19 +0200

    chore: trigger CI

--- ressources/s5/noheila-ra/projets/ci-cd-symfony/tests/bootstrap.php
+++ ressources/s5/noheila-ra/projets/ci-cd-symfony/tests/bootstrap.php
@@ -6,6 +6,12 @@ require dirname(__DIR__).'/vendor/autoload.php';
 
 if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
     require dirname(__DIR__).'/config/bootstrap.php';
-} elseif (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+} else {
+    $dotenv = new Dotenv();
+    // essaie .env.test d'abord, sinon fallback sur .env
+    if (file_exists(dirname(__DIR__).'/.env.test')) {
+        $dotenv->loadEnv(dirname(__DIR__).'/.env.test');
+    } else {
+        $dotenv->bootEnv(dirname(__DIR__).'/.env');
+    }
 }
